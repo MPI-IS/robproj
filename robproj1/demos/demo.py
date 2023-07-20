79eb111d130570945043042dabaed15975395fa4
Vincent Berenz <vincent.berenz@tuebingen.mpg.de>
2023-07-20 17:16:40 +0200

    added comments to robproj1 demos

--- robproj1/demos/demo.py
+++ robproj1/demos/demo.py
@@ -1,6 +1,17 @@
 import time
 from robproj1 import CosSignal
 
+# The class CosSignal is a C++ class
+# (see robproj1/include/robproj1/cos_signal.hpp)
+# which has been binded for python. See
+# robproj1/srcpy/wrappers.cpp and robproj1/CMakeLists.txt
+# (but robproj2 does something similar and its CMakeLists.txt
+# file has more comments)
+
+# Here you may see these bindings in action.
+# To run this demo, simply run:
+# 'python ./demo.py'
+
 PERIOD = 1.0
 SLEEP = 0.5
 
